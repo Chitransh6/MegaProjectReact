900702bde68ca5b84a8988f18d7c25668d49106f
Chitransh6 <chitranshsharma067@gmail.com>
2026-01-29 18:46:48 +0530

    third class of mega project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,49 @@
 import './App.css'
+import { useState,useEffect } from 'react';
+import authService from "./appwrite/auth"
+import { useDispatch } from 'react-redux';
+import {login,logout} from "./store/authSlice"
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
- return(
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  <p> A Blog App with Appwrite
-  </p>
+  useEffect(()=>{
+   authService.getCurrentUser()
+   .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}));
+    } else{
+      dispatch(logout());
+    }
+   })
 
- )
+   .finally(()=>setLoading(false))
+  },[])
+
+ if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      {/* Your Header, Outlet (for routing), and Footer would go here */}
+     <div className='w-full block'>
+       <Header/>
+       <main>
+        <Outlet/>
+        TODO:
+       </main>
+       <Footer/>
+     </div>
+    </div>
+  );
 }
 
 export default App
