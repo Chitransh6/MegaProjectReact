b1a1e45c36fa5678794e1ac87a3584062298324e
Chitransh6 <chitranshsharma067@gmail.com>
2026-02-15 15:13:34 +0530

    logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ function Logo({width = '100px'}) {
   return (
     <div>
     <img
-    src="https://www.pexels.com/photo/close-up-of-silver-emblem-17357682/" alt="logo" />
+    src="https://images.pexels.com/photos/17357682/pexels-photo-17357682.jpeg?_gl=1*12a7ry4*_ga*NDk3NTQ1MTM5LjE3NjY5MTQzNjQ.*_ga_8JE65Q40S6*czE3NzExNDc3NTUkbzUkZzEkdDE3NzExNDgzOTIkajMyJGwwJGgw" alt="logo" />
     </div>
   )
 }
