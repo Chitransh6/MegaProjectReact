2c040dded3be66aa9817b17172d9e03bab41a7f8
Chitransh6 <chitranshsharma067@gmail.com>
2026-02-15 15:19:54 +0530

    n

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,8 +5,7 @@ function Logo({width = '100px'}) {
     <div>
     <img
     src="https://images.pexels.com/photos/17357682/pexels-photo-17357682.jpeg?_gl=1*12a7ry4*_ga*NDk3NTQ1MTM5LjE3NjY5MTQzNjQ.*_ga_8JE65Q40S6*czE3NzExNDc3NTUkbzUkZzEkdDE3NzExNDgzOTIkajMyJGwwJGgw" alt="logo"
-    style={{ width: width }} // This line forces the image to use the prop
-    className="rounded-full shadow-sm"
+    className={`${width} h-auto object-contain rounded-lg`}
          />
     </div>
   )
